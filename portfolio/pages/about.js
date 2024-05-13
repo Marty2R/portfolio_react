817ecd6f39fcc696e56e4c39fb09ef6e21a84628
Marty Rabatel <marty.rabatel2@gmail.com>
2024-05-13 15:28:06 +0200

    ADD HOME PAGE AND ABOUT PAGE

---
+++ portfolio/pages/about.js
@@ -0,0 +1,52 @@
+import Header from "@/componant/Header";
+
+export default function About() {
+  return (
+    <>
+      <Header></Header>
+
+      <main>
+        <section>
+          <div>
+            <h1 className="title">About me</h1>
+          </div>
+          <div>
+            <h2>My projects</h2>
+            <div>
+              <div>
+                <h3>Site de e-commerce</h3>
+                <p>
+                  lorem ispum lorem ispum lorem ispum lorem ispum lorem ispum
+                  lorem ispum lorem ispum lorem ispum lorem ispum
+                </p>
+              </div>
+              <div>
+                <h3>Algorythme de trie</h3>
+                <p>
+                  lorem ispum lorem ispum lorem ispum lorem ispum lorem ispum
+                  lorem ispum lorem ispum lorem ispum lorem ispum
+                </p>
+              </div>
+              <div>
+                <h3>Maquettes</h3>
+                <p>
+                  lorem ispum lorem ispum lorem ispum lorem ispum lorem ispum
+                  lorem ispum lorem ispum lorem ispum lorem ispum
+                </p>
+              </div>
+            </div>
+            <h2>My job</h2>
+            <div>
+              <p>
+                lorem ispum lorem ispum lorem ispum lorem ispum lorem ispum
+                lorem ispum lorem ispum lorem ispum lorem ispum lorem ispum
+                lorem ispum lorem ispum lorem ispum lorem ispum lorem ispum
+                lorem ispum lorem ispum lorem ispum
+              </p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
---
+++ portfolio/pages/index.js
@@ -0,0 +1,25 @@
+import Img from "@/componant/Img";
+import Header from "@/componant/Header";
+import Head from "@/componant/Head";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <div>
+          <h1>
+            Hi !
+            <br />
+            I'm <span>Nooman</span>,
+            <br />
+            I'm a developer.
+          </h1>
+          <button>Contact</button>
+        </div>
+        <Img />
+      </main>
+    </>
+  );
+}
